261780101628f66de42aafa592470018187a369b
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2024-12-20 11:57:55 +0100

    feat: add gemini factory

--- src/main/scala/it/unibo/scafi/GeminiService.scala
+++ src/main/scala/it/unibo/scafi/GeminiService.scala
@@ -2,10 +2,11 @@ package it.unibo.scafi
 
 import requests.*
 
-class GeminiService(val apiKey: String, override val localKnowledge: String) extends CodeGeneratorService:
+class GeminiService(val model: String, val apiKey: String, override val localKnowledge: String)
+    extends CodeGeneratorService:
 
   override def generateCode(prompt: String): String =
-    val url = s"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$apiKey"
+    val url = s"https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=$apiKey"
     val headers = Map("Content-Type" -> "application/json")
     val data =
       s"""{
@@ -17,3 +18,20 @@ class GeminiService(val apiKey: String, override val localKnowledge: String) ext
     val response = requests.post(url, headers = headers, data = data)
     // convert the response from byte to string
     response.text()
+
+object GeminiService:
+
+  enum Version:
+    case V1_5
+    case V2_0
+
+    override def toString: String =
+      this match
+        case V1_5 => "1.5"
+        case V2_0 => "2.0"
+
+  def flash(version: Version, apiKey: String, localKnowledge: String): GeminiService =
+    new GeminiService(s"gemini-$version-flash", apiKey, localKnowledge)
+
+  def pro(version: Version, apiKey: String, localKnowledge: String): GeminiService =
+    new GeminiService(s"gemini-$version-pro", apiKey, localKnowledge)
