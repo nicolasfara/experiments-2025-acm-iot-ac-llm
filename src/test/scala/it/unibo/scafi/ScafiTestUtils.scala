27d3de43e098320bd17894091fa9ad9762e16729
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-01-20 10:04:17 +0100

    test: add channel program

--- src/test/scala/it/unibo/scafi/ScafiTestUtils.scala
+++ src/test/scala/it/unibo/scafi/ScafiTestUtils.scala
@@ -58,6 +58,17 @@ object ScafiTestUtils:
          |        7 -> false,
          |        8 -> false,
          |      ),
+         |      "destination" -> Map(
+         |        0 -> false,
+         |        1 -> false,
+         |        2 -> false,
+         |        3 -> false,
+         |        4 -> false,
+         |        5 -> false,
+         |        6 -> false,
+         |        7 -> false,
+         |        8 -> true,
+         |      ),
          |    ),
          |  )
          |
