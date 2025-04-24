bda63893c1d4e59c447d1ecdd0ca306c2e1151a8
Davide Boldini <davide.boldini@studio.unibo.it>
2025-04-24 15:49:38 +0200

    fork fix

--- src/main/scala/it/unibo/scafi/CodeGeneratorService.scala
+++
@@ -1,7 +0,0 @@
-package it.unibo.scafi
-
-import scala.concurrent.{ExecutionContext, Future}
-
-trait CodeGeneratorService:
-  def generateMain(localKnowledge: String, prompt: String): ExecutionContext ?=> Future[String]
-  def generateRaw(localKnowledge: String, preamble: String, prompt: String): ExecutionContext ?=> Future[String]
